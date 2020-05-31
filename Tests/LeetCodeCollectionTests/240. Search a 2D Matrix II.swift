093b2a6da51c039beb3ada8c5bbe5ae9bb07b7b2
Tsungyu Yu <ytyubox@gmail.com>
2020-05-31 16:00:29 +0800

    add 240 test case

---
+++ Tests/LeetCodeCollectionTests/240. Search a 2D Matrix II.swift
@@ -0,0 +1,43 @@
+private class Solution {
+    func searchMatrix(_ matrix: [[Int]], _ target: Int) -> Bool {
+        false
+    }
+}
+
+
+import XCTest
+/**
+ Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+ Integers in each row are sorted in ascending from left to right.
+ Integers in each column are sorted in ascending from top to bottom.
+ Example:
+
+ Consider the following matrix:
+
+ [
+   [1,   4,  7, 11, 15],
+   [2,   5,  8, 12, 19],
+   [3,   6,  9, 16, 22],
+   [10, 13, 14, 17, 24],
+   [18, 21, 23, 26, 30]
+ ]
+ Given target = 5, return true.
+
+ Given target = 20, return false.
+ */
+class _240Searcha2DMatrix6II:XCTestCase {
+    let solution = Solution()
+    func testExampleCase() {
+        let matrix =  [
+          [1,   4,  7, 11, 15],
+          [2,   5,  8, 12, 19],
+          [3,   6,  9, 16, 22],
+          [10, 13, 14, 17, 24],
+          [18, 21, 23, 26, 30]
+        ]
+        
+        let output = solution.searchMatrix(matrix, 5)
+        XCTAssertTrue(output)
+    }
+}
