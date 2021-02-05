f9ec97ebe3cc7ea4f57cdeb94589c83aaf950023
Tsungyu Yu <ytyubox@gmail.com>
2021-02-05 23:36:00 +0800

    update

--- Sources/LeetCodeCollection/2. Add Two Numbers.swift
+++ Sources/LeetCodeCollection/2. Add Two Numbers.swift
@@ -7,39 +7,6 @@
 
 import Foundation
 
-public class ListNode {
-  /// Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
-  /// Output: 7 -> 0 -> 8
-  /// Explanation: 342 + 465 = 807.
-  
-  public var val: Int
-  public var next: ListNode?
-  public init(_ val: Int) {
-    self.val = val
-    self.next = nil
-  }
-    public init(_ val: Int, next: ()->ListNode?) {
-      self.val = val
-      self.next = next()
-    }
-  
-}
-extension ListNode {
-  public static func make(array: [Int]) -> ListNode {
-    if array.isEmpty {fatalError()}
-    var list = array
-    let returnNode = ListNode(list.removeFirst())
-    var node:ListNode? = returnNode
-    for v in list {
-      node?.next = ListNode(v)
-      node = node?.next
-    }
-    return returnNode
-  }
-}
-public extension ListNode {
-    var id: ObjectIdentifier {.init(self)}
-}
 
 class AddTwoNumbersSolution: Run {
   
--- Sources/LeetCodeCollection/LeetCodeCollection.swift
+++ Sources/LeetCodeCollection/LeetCodeCollection.swift
@@ -1,3 +1,4 @@
+import Foundation
 struct LeetCodeCollection {
     var text = "Hello, World!"
 }
