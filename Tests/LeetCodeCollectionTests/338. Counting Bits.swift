a143250bb3b327a4018342db56adbe3d76a3591f
Tsungyu Yu <ytyubox@gmail.com>
2021-09-21 19:38:31 +0800

    338

--- Tests/LeetCodeCollectionTests/338. Counting Bits.swift
+++ Tests/LeetCodeCollectionTests/338. Counting Bits.swift
@@ -1,7 +1,21 @@
 import Foundation
 private class Solution {
     func countBits(_ n: Int) -> [Int] {
-       []
+        var r:[Int] = []
+        for i in 0...n {
+            r.append(get(n: i))
+            
+        }
+       return r
+    }
+    private func get(n: Int) -> Int {
+        var n = n
+        var count = 0
+        while true {
+            if n == 0 {return count}
+            count += n & 1
+            n >>= 1
+        }
     }
 }
 
@@ -9,7 +23,8 @@ import XCTest
 
 final class _338Tests: XCTestCase {
     func test() throws {
-        XCTFail("this test should fail if your scheme contain this test")
+        XCTAssertEqual(Solution().countBits(2), [0,1,1])
+        XCTAssertEqual(Solution().countBits(5), [0,1,1,2,1,2])
     }
 }
 
